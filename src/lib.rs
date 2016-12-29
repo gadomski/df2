4f432de3db0be0846cb70eb537a7f0b41fb78b7b
Pete Gadomski <pete.gadomski@gmail.com>
2016-12-29 10:59:15 -0700

    Add lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 /// Df2 files are waveform files from an Optech LiDAR system.
+#[deny(missing_docs,
+        missing_debug_implementations, missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
 
 extern crate byteorder;
 extern crate rustc_serialize;
