eedc049604c366982a5501ed7db9cbc2cc17569d
Pete Gadomski <pete.gadomski@gmail.com>
2016-12-29 09:55:39 -0700

    Read file

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@ use byteorder::{LittleEndian, ReadBytesExt};
 /// Our custom error enum.
 #[derive(Debug)]
 pub enum Error {
+    /// Shot has an invalid offset.
+    InvalidOffset { shot_number: u16, offset: u16 },
+    /// Wrapper around `std::io::Error`.
     Io(io::Error),
 }
 
@@ -55,6 +58,7 @@ impl<R: Read> Reader<R> {
     /// let shot = reader.read_one().unwrap().unwrap();
     /// ```
     pub fn read_one(&mut self) -> Result<Option<Shot>> {
+        // FIXME this isn't exactly correct, a spare byte could be allowed
         let number = match self.reader.read_u16::<LittleEndian>() {
             Ok(number) => number,
             Err(err) => {
@@ -64,7 +68,27 @@ impl<R: Read> Reader<R> {
                 }
             }
         };
-        Ok(Some(Shot { number: number }))
+        let offset = self.reader.read_u16::<LittleEndian>()?;
+        let mut bytes_remaining = offset * 2;
+        let outgoing = Segment::from_read(&mut self.reader)?;
+        bytes_remaining -= outgoing.len();
+        let mut segments = Vec::new();
+        while bytes_remaining > 0 {
+            let segment = Segment::from_read(&mut self.reader)?;
+            if segment.len() > bytes_remaining {
+                return Err(Error::InvalidOffset {
+                    shot_number: number,
+                    offset: offset,
+                });
+            }
+            bytes_remaining -= segment.len();
+            segments.push(segment);
+        }
+        Ok(Some(Shot {
+            number: number,
+            outgoing: outgoing.data,
+            segments: segments,
+        }))
     }
 }
 
@@ -87,6 +111,70 @@ impl<R: Read> Iterator for Reader<R> {
 pub struct Shot {
     /// The shot number (one-indexed).
     pub number: u16,
+    /// The outgoing laser pulse.
+    pub outgoing: Vec<u16>,
+    /// The waveform segments.
+    pub segments: Vec<Segment>,
+}
+
+/// A waveform segment.
+pub struct Segment {
+    /// The waveform samples.
+    pub data: Vec<u16>,
+    /// The time interval, in cycles.
+    pub time_interval: u16,
+}
+
+impl Segment {
+    /// Reads a Segment from a path.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use df2::Segment;
+    /// let segment = Segment::from_path("data/one-segment.bin").unwrap();
+    /// ```
+    pub fn from_path<P: AsRef<Path>>(path: P) -> Result<Segment> {
+        File::open(path)
+            .map_err(|err| err.into())
+            .and_then(|mut file| Segment::from_read(&mut file))
+    }
+
+    /// Reads a Segment from a `Read`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::fs::File;
+    /// use df2::Segment;
+    /// let mut file = File::open("data/one-segment.bin").unwrap();
+    /// let segment = Segment::from_read(&mut file).unwrap();
+    /// ```
+    pub fn from_read<R: Read>(read: &mut R) -> Result<Segment> {
+        let nsamples = read.read_u16::<LittleEndian>()?;
+        let data = (0..nsamples).map(|_| read.read_u16::<LittleEndian>().map_err(|err| err.into()))
+            .collect::<Result<Vec<u16>>>()?;
+        let time_interval = read.read_u16::<LittleEndian>()?;
+        // reserved
+        let _ = read.read_u16::<LittleEndian>()?;
+        Ok(Segment {
+            data: data,
+            time_interval: time_interval,
+        })
+    }
+
+    /// Returns the length of this segment in bytes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use df2::Segment;
+    /// let segment = Segment::from_path("data/one-segment.bin").unwrap();
+    /// assert_eq!(110, segment.len());
+    /// ```
+    pub fn len(&self) -> u16 {
+        self.data.len() as u16 * 2 + 6
+    }
 }
 
 #[cfg(test)]
@@ -109,5 +197,16 @@ mod tests {
     fn reader_iterator() {
         let shots: Vec<_> = Reader::from_path("data/one-shot.df2").unwrap().collect();
         assert_eq!(1, shots.len());
+    }
+
+    #[test]
+    fn segment_from_path() {
+        assert!(Segment::from_path("data/one-segment.bin").is_ok());
+    }
+
+    #[test]
+    fn segment_len() {
+        let segment = Segment::from_path("data/one-segment.bin").unwrap();
+        assert_eq!(110, segment.len());
     }
 }
