0b2e96b45ac173fe6915e6ff32f9879ff214440e
Pete Gadomski <pete.gadomski@gmail.com>
2016-12-29 08:31:55 -0700

    Add basic reader

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,38 @@
+/// Df2 files are waveform files from an Optech LiDAR system.
+
+use std::path::Path;
+
+/// Our custom error enum.
+#[derive(Debug)]
+pub enum Error {}
+
+/// Our custom result type.
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// Reads df2 waveform data.
+#[derive(Debug)]
+pub struct Reader;
+
+impl Reader {
+    /// Opens a reader for the file at a path.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use df2::Reader;
+    /// let reader = Reader::from_path("../data/one-shot.df2").unwrap();
+    /// ```
+    pub fn from_path<P: AsRef<Path>>(_: P) -> Result<Reader> {
+        Ok(Reader)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn reader_from_path() {
+        assert!(Reader::from_path("../data/one-shot.df2").is_ok());
     }
 }
